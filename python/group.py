304dea6fcea1322db35773fb0617fc7b81e03cb8
Artem Werner <artem-werner@t-online.de>
2017-10-22 14:16:46 +0200

    new method to find generators

--- python/group.py
+++ python/group.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# https://pythoniter.appspot.com/
+
 import math
 import json
 
 from fractions import gcd
 
-# Return a list of coprimes for a digit
+
+# Return a list of coprimes
 def coprime(a):
     b = a - 1
     l = []
@@ -20,28 +23,49 @@ def coprime(a):
 
     return l
 
-# Return subgroups of a group specified as list
+
+# Return subgroups as dictionary
 def subgroup(a):
     d = {}
     l = coprime(a)
     ls = len(l)
 
     for e in l:
-        s = []
-        d[e] = s
+        d[e] = []
 
         for i in range(ls):
             ee = math.pow(e, i) % a
 
             if ee in l:
-                if ee not in s:
-                    s.append(int(ee))
+                if ee not in d[e]:
+                    d[e].append(int(ee))
+
+        d[e].sort()
 
     return d
 
-# Pretty print specified content
+
+# Returns generators
+def generator(a):
+    g = []
+    c = coprime(a)
+    s = subgroup(a)
+
+    for (k, v) in s.iteritems():
+        if c == v:
+            g.append(k)
+
+    return g
+
+
+# Returns length of a content
+def l(l):
+    return len(l)
+
+
+# Pretty print specified dictionary
 def p(a):
-    for k, v in a.iteritems():
+    for (k, v) in a.iteritems():
         v.sort()
 
     print json.dumps(a, sort_keys=True)
